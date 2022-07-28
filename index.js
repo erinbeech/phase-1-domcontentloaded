be6888c36d071ee0ff328fc83e0622e8890d7475
brian <erinbeech@icloud.com>
2022-07-28 10:40:51 -0400

    commit tree e91b529a095c5266e670aa5b9e8a1cc2a933a245

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Your code goes here
+
+const text = document.querySelector('#text')
+function updateInner() {
+    text.innerHTML = "This is really cool!"
+}
+
+function alertClick() {
+    text.innerHTML = 'Hey, you find the Default Text: "JavaScript is so cool it lets me add text to my page programmaticaly."'
+
+}
+
+document.addEventListener('DOMContentLoaded', updateInner);
+
+text.addEventListener('click', alertClick);
